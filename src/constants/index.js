06e581fb830258aaf5a7eb93886218bed91b0867
abduljabar160 <apgarces.ccit@unp.edu.ph>
2024-09-11 20:38:42 +0800

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,35 @@
+import project1 from "../assets/projects/project-1.png";
+// import project2 from "../assets/projects/project-2.jpg";
+// import project3 from "../assets/projects/project-3.jpg";
+// import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications.I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like ReactNative (expo), Node.js, MySQL, and Firebase. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Next.js, Java, Node.js, MySQL, and Firebase. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2022 - 2023",
+    role: "On The Job Training (OJT)",
+    company: "Network System Operating Services",
+    description: `During my on-the-job training, I was responsible for configuring and setting up a network infrastructure for a university. My tasks included installing, configuring, and maintaining network devices such as routers, switches, and firewalls. I provided hands-on support for the university's server, ensuring optimal performance and resolving any technical issues. This experience deepened my understanding of network security, server management, and troubleshooting, while enhancing my ability to collaborate effectively within a technical support team.`,
+    technologies: ["None"],
+  },
+
+];
+
+export const PROJECTS = [
+  {
+    title: "BloodBank Finder in Ilocos Sur",
+    image: project1,
+    description:
+      "A fully functional app for who needs a blood in Ilocos sur, this is use to request or donate a blood in respected BloodBanks our area",
+    technologies: ["ReactNative", "NodeJs Express"],
+  },
+];
+
+export const CONTACT = {
+  address: "Sheridan Towers, North Tower, Mandaluyong City",
+  phoneNo: "+639212091010 ",
+  email: "alexgarces1201@gmail.com",
+};
